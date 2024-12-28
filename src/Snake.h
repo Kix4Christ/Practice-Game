26e08e7933f3815a481c916979d89aa6b17a9966
your name <your email>
2024-12-27 22:29:05 -0500

    AHHHHH WHY IS DIR NOT INITIALIZED IDSONFSHEIOGHSD

--- src/Snake.h
+++ src/Snake.h
@@ -1,4 +1,6 @@
+#pragma once
 #include <SFML/Graphics.hpp>
+#include "Direction.h"
 
 class Snake
 {
@@ -8,13 +10,24 @@ private:
 		sf::Vector2i position;
 		Direction dir;
 
+		segment()
+		{
+			position = sf::Vector2i{ 0,0 };
+			dir = Direction::right;
+		}
 	}head;
 
 	std::vector<segment> segments;
 	
 	int length;
+
+	sf::Vector2i dirToVector(Direction dir);
+	Direction reverseDir(Direction dir);
+
 public:
 	Snake();
 	~Snake();
+	void move();
 	void move(Direction dir);
+	void increaseLength();
 };
